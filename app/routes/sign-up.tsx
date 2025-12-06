2e93bf2edb2e5f3e32ef51808851cbb4ef687dc4
Brodie <bugbrodie@gmail.com>
2025-12-06 22:14:03 +0800

    clerk login page troubleshoot

--- app/routes/sign-up.tsx
+++ app/routes/sign-up.tsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/react-router";
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <SignUp />
+      <SignUp 
+        routing="path" 
+        path="/sign-up" 
+        signInUrl="/sign-in"
+        afterSignUpUrl="/dashboard"
+      />
     </div>
   );
 }
